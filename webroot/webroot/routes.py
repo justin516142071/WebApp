7b023163abe2340fe71bcad0ec1c63b180a86f5c
justin Liang <justin516142071@gmail.com>
2019-12-01 16:18:43 -0500

    fix admin access5

--- webroot/webroot/routes.py
+++ webroot/webroot/routes.py
@@ -93,10 +93,10 @@ def history():
 @app.route("/history/query<int:queryid>")
 @login_required
 def query(queryid):
-    if(current_user!= Query.query.get_or_404(queryid - 5).user and current_user.role != 'Admin'):
-        return make_response("Unauthorized", 401)
+    if(current_user!= Query.query.get_or_404(queryid).user and current_user.role != 'Admin'):
+        return render_template('query.html', title='Query')
     else:
-        query = Query.query.get_or_404(queryid - 5)
+        query = Query.query.get_or_404(queryid)
         return render_template('query.html', title='Query', post=query)
 
 @app.route("/login_history", methods=['GET','POST'])
